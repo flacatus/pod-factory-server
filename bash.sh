1ea49b9a3fbe4d7d273a83ae569984d2f5d26fbb
flacatus <flacatus@redhat.com>
2020-04-13 14:21:14 +0200

    Tests
    Signed-off-by: flacatus <flacatus@redhat.com>

--- bash.sh
+++ bash.sh
@@ -18,17 +18,12 @@ echo ${PR_FILES_CHANGED}
 # in case of modification should exist also modifications in deploy/crds/* folder.
 function check_che_types() {
     # CHE_TYPES_FILE make reference to generated code by operator-sdk.
-    local CHE_TYPES_FILE='\S*/apis/org/v1/che_types.go'
+    local CHE_TYPES_FILE='\S*che_types.go'
     # Export variables for cr/crds files.
     local CR_CRD_FOLDER="deploy/crds/"
     local CR_CRD_REGEX="\S*org_v1_che_crd.yaml"
     echo ${PR_FILES_CHANGED[@]}
-if [[ " ${PR_FILES_CHANGED[@]} " =~ $CR_CRD_REGEX ]]; then
-            echo "[INFO] CR/CRD file modified: ${BASH_REMATCH}"
-        else
-            echo "[ERROR] Detected modification in ${CHE_TYPES_FILE} file, but cr/crd files didn't suffer any modification."
-            exit 1
-        fi    
+   
     if [[ " ${PR_FILES_CHANGED[@]} " =~ " ${CHE_TYPES_FILE} " ]]; then
         echo "[INFO] File ${CHE_TYPES_FILE} suffer modifications in PR. Checking if exist modifications for cr/crd files."
         # The script should fail if deploy/crds folder didn't suffer any modification.
